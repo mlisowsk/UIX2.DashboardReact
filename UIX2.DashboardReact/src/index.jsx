c09a2683835892a194fd2ae023e89fa7d84a8050
Martin Lisowski <martin.j.lisowski@gmail.com>
2024-08-27 21:06:01 +0200

    Added "restoring default content" button

--- UIX2.DashboardReact/src/index.jsx
+++ UIX2.DashboardReact/src/index.jsx
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom'
 function App(dashboard) {
   const shellFrame = dashboard.ShellFrame
 
+  const showDefaultContent = () => {
+    shellFrame.ShowDefaultContent()
+  }
+
   return (
     <div>
       <h1>My dashboard</h1>
       <div>
         Current path: <span class="label">{shellFrame.CurrentPath}</span>
       </div>
+      <button type="button" onClick={showDefaultContent}>
+        Show default Content
+      </button>
     </div>
   )
 }
